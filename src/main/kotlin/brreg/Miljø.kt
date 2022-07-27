19dcb6bc08b4f485c0cc8fdcec32c349eb030fd3
Stefan A. Engelien <stefan.andreas.engelien@nav.no>
2022-07-27 13:39:25 +0200

    Overstyrer Kafka-sikkerhetsinnstillinger for test
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>

--- src/main/kotlin/brreg/Miljø.kt
+++ src/main/kotlin/brreg/Miljø.kt
@@ -4,9 +4,9 @@ import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringSerializer
+import java.io.Serializable
 
 object Miljø {
-    val CLUSTER = getEnvVar("NAIS_CLUSTER_NAME", "lokal")
     val ANTALL_DAGER_SIDEN_OPPDATERING = getEnvVar("ANTALL_DAGER_SIDEN_OPPDATERING", "1")
     val KAFKA_BROKERS = getEnvVar("KAFKA_BROKERS", "")
     val KAFKA_TRUSTSTORE_PATH = getEnvVar("KAFKA_TRUSTSTORE_PATH", "")
@@ -14,14 +14,24 @@ object Miljø {
     val KAFKA_CREDSTORE_PASSWORD = getEnvVar("KAFKA_CREDSTORE_PASSWORD", "")
     const val KAFKA_TOPIC = "pia.brreg-oppdatering"
 
-    fun producerProperties() =
-        mapOf(
+    fun producerProperties(): Map<String, Serializable> {
+        val producerConfigs = mutableMapOf(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to KAFKA_BROKERS,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true, // Den sikrer rekkefølge
             ProducerConfig.ACKS_CONFIG to "all", // Den sikrer at data ikke mistes
-            ProducerConfig.CLIENT_ID_CONFIG to "pia-brreg",
+            ProducerConfig.CLIENT_ID_CONFIG to "pia-brreg"
+        )
+
+        if (KAFKA_TRUSTSTORE_PATH.isNotEmpty()) {
+            producerConfigs.putAll(securityConfigs())
+        }
+        return producerConfigs.toMap()
+    }
+
+    private fun securityConfigs() =
+        mapOf(
             CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "SSL",
             SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "",
             SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to "JKS",
@@ -33,8 +43,6 @@ object Miljø {
             SslConfigs.SSL_KEY_PASSWORD_CONFIG to KAFKA_CREDSTORE_PASSWORD
         )
 }
-
-
 
 
 private fun getEnvVar(envVar: String, default: String? = null) =
