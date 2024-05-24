9d6e9529420641cf49972adee89ba0094c3361c5
nimajimale <nima0512@gmail.com>
2024-05-24 12:49:36 +0200

    Oppdater avhengigheter. * Venter med å oppdatere kotlin til 2.0.0 (K2) da den er for ny

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
-    val ktorVersion = "2.3.9"
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+    val ktorVersion = "2.3.11"
     implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
@@ -22,18 +22,18 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
     implementation("org.apache.kafka:kafka-clients:3.7.0")
-    implementation("ch.qos.logback:logback-classic:1.5.3")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
     implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
-    val testcontainersVersion = "1.19.7"
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    val testcontainersVersion = "1.19.8"
     testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
     testImplementation("org.testcontainers:kafka:$testcontainersVersion")
-    val kotestVersion = "5.8.1"
+    val kotestVersion = "5.9.0"
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-json:$kotestVersion")
 
@@ -51,7 +51,7 @@ dependencies {
         }
         testImplementation("org.apache.commons:commons-compress") {
             version {
-                require("1.26.1")
+                require("1.26.2")
             }
             because("testcontainers har sårbar versjon")
         }
