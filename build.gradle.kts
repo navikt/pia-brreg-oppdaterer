afb404b2f6fc8547e81079e25a34a054c02ad34e
Christian Klem <christian.klem@nav.no>
2024-06-20 09:36:19 +0200

    Diverse oppdateringer
    - kotlin til 2.0.0
    - kotest minor version
    - fjern unødvendig constraint

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,17 +38,6 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-json:$kotestVersion")
 
     constraints {
-        testImplementation("com.jayway.jsonpath:json-path") {
-            version {
-                require("2.9.0")
-            }
-            because(
-                """
-                    json-path v2.8.0 was discovered to contain a stack overflow via the Criteria.parse() method.
-                    introdusert gjennom io.kotest:kotest-assertions-json:5.8.0
-                """.trimIndent()
-            )
-        }
         testImplementation("org.apache.commons:commons-compress") {
             version {
                 require("1.26.2")
