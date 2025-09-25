f84b214b772f5a17d046fb1fba841fd2ed68f99f
Christian Klem <christian.klem@nav.no>
2025-09-25 13:35:26 +0200

    Legg til støtte for postadresse  for de virksomheter som ikke har beliggenhetsadresse
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- src/main/kotlin/brreg/BrregApi.kt
+++ src/main/kotlin/brreg/BrregApi.kt
@@ -4,34 +4,40 @@ import kotlinx.serialization.Serializable
 import java.time.ZonedDateTime
 
 interface BrregApi {
-    suspend fun hentOppdaterteUnderenheter(tidspunkt: ZonedDateTime, oppdateringsId: Long?, side: Int): BrregOppdateringDTO
+    suspend fun hentOppdaterteUnderenheter(
+        tidspunkt: ZonedDateTime,
+        oppdateringsId: Long?,
+        side: Int,
+    ): BrregOppdateringDTO
+
     suspend fun hentUnderenheter(orgnummere: List<String>): List<BrregVirksomhetDto>
 }
 
 @Serializable
 data class BrregVirksomhetDto(
-    val organisasjonsnummer: String,
-    val navn: String,
-    val oppstartsdato: String? = null,
-    val beliggenhetsadresse: Beliggenhetsadresse? = null,
-    val naeringskode1: NæringsundergruppeBrreg? = null,
-    val naeringskode2: NæringsundergruppeBrreg? = null,
-    val naeringskode3: NæringsundergruppeBrreg? = null,
-    )
+	val organisasjonsnummer: String,
+	val navn: String,
+	val oppstartsdato: String? = null,
+	val beliggenhetsadresse: Adresse? = null,
+	val postadresse: Adresse? = null,
+	val naeringskode1: NæringsundergruppeBrreg? = null,
+	val naeringskode2: NæringsundergruppeBrreg? = null,
+	val naeringskode3: NæringsundergruppeBrreg? = null,
+)
 
 @Serializable
 data class NæringsundergruppeBrreg(
     val beskrivelse: String,
-    val kode: String
+    val kode: String,
 )
 
 @Serializable
-data class Beliggenhetsadresse(
+data class Adresse(
     val land: String? = null,
     val landkode: String? = null,
     val postnummer: String? = null,
     val poststed: String? = null,
     val adresse: List<String>? = null,
     val kommune: String? = null,
-    val kommunenummer: String? = null
+    val kommunenummer: String? = null,
 )
