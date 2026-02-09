6bfb0f9f592c919142e6a4e5fe751304ad0af228
Christian Klem <christian.klem@nav.no>
2026-02-09 11:10:32 +0100

    Nedgrader kotlin til 2.3.0 da codeql ikke støtter nyeste

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ val wiremockVersion = "3.13.2"
 
 plugins {
     application
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.serialization") version "2.3.10"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
 }
 
 group = "no.navikt"
